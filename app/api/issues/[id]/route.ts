77b82e6527a7569b92a1ebbd940dc45728563dee
devmarkos <valiskomark@icloud.com>
2023-10-14 15:22:36 +0200

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,29 +1,48 @@
-import prisma from '@/prisma/client';
+import prisma from "@/prisma/client";
 import { issueSchema } from "@/app/validationSchemas";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
-    request: NextRequest, 
-    {params}: {params: {id: string}} ){
-    const body = await request.json();
-    const validation = issueSchema.safeParse(body);
-    if (!validation.success)
-        return NextResponse.json(validation.error.format(), {status: 400});
-    
-    const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(params.id)}
-    });
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
-    if (!issue) 
-        return NextResponse.json({error: 'Invalid issue'}, {status: 404});
-    
-    const updatedIssue = await prisma.issue.update({
-        where: {id: issue.id},
-        data: {
-            title: body.title,
-            description: body.description
-        }
-    })
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
-    return NextResponse.json(updatedIssue);
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  await prisma.issue.delete({
+    where: { id: issue.id },
+  });
+
+  return NextResponse.json({});
 }
